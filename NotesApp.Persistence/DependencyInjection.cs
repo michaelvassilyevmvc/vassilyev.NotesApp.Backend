9fdb39d76f3a5d85f03be48162de5bbe85fcfd12
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2022-12-18 13:32:32 +0600

    Setting DI for Application and Persistence

---
+++ NotesApp.Persistence/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NotesApp.Application.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace NotesApp.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<NotesDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+
+            services.AddScoped<INotesDbContext>(provider => provider.GetService<INotesDbContext>());
+            return services;
+        }
+    }
+}
